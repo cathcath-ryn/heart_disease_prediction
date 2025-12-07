11799ceae703c61d935274bf1fb7389730cfddda
Ei Phyu Sin Win <eiphyu.sinwin29@gmail.com>
2025-12-07 21:46:40 +0630

    Update heart_diseaseapp.py

--- heart_diseaseapp.py
+++ heart_diseaseapp.py
@@ -12,7 +12,7 @@ if os.path.exists(logo_path):
     st.sidebar.image(logo_path, width=150)
 
 
-st.sidebar.markdown("Name:**Ei Phyu Sin Win**")
+st.sidebar.markdown("Name: **Ei Phyu Sin Win**")
 st.sidebar.markdown("**Student ID: PIUS20230033**")
 st.sidebar.markdown("Class: 2027")
 st.sidebar.markdown("Intro to Machine Learning")
@@ -24,26 +24,26 @@ sex = st.radio("Sex", ["Female", "Male"])
 sex = 1 if sex == "Male" else 0
 cp = st.selectbox(
     "Chest Pain Type",
-    ["Typical Angina (0)", "Atypical Angina (1)", "Non-anginal Pain (2)", "Asymptomatic (3)"]
+    ["Typical Angina", "Atypical Angina", "Non-anginal Pain", "Asymptomatic"]
 )
 cp = int(cp[-2])
 trestbps = st.number_input("Resting Blood Pressure", 80, 200, 120)
 chol = st.number_input("Cholesterol", 100, 600, 200)
-fbs = st.selectbox("Fasting Blood Sugar > 120 mg/dl", ["No (0)", "Yes (1)"])
+fbs = st.selectbox("Fasting Blood Sugar > 120 mg/dl", ["No", "Yes"])
 fbs = int(fbs[-2])
 restecg = st.selectbox(
     "Resting ECG Results",
-    ["Normal (0)", "ST-T wave abnormality (1)", "Left ventricular hypertrophy (2)"]
+    ["Normal", "ST-T wave abnormality", "Left ventricular hypertrophy"]
 )
 restecg = int(restecg[-2])
 thalach = st.number_input("Max Heart Rate Achieved", 60, 220, 150)
-exang = st.selectbox("Exercise Induced Angina", ["No (0)", "Yes (1)"])
+exang = st.selectbox("Exercise Induced Angina", ["No", "Yes"])
 exang = int(exang[-2])
 oldpeak = st.number_input("Oldpeak", 0.0, 7.0, 1.0)
-slope = st.selectbox("ST Slope", ["Upsloping (0)", "Flat (1)", "Downsloping (2)"])
+slope = st.selectbox("ST Slope", ["Upsloping", "Flat", "Downsloping"])
 slope = int(slope[-2])
-ca = st.slider("Number of Visible Major Vessels (0–3)", 0, 3, 0)
-thal = st.selectbox("Thalassemia", ["Normal (1)", "Fixed defect (2)", "Reversible defect (3)"])
+ca = st.slider("Number of Visible Major Vessels", 0, 3, 0)
+thal = st.selectbox("Thalassemia", ["Normal", "Fixed defect", "Reversible defect"])
 thal = int(thal[-2])
 
 if st.button("Predict"):
